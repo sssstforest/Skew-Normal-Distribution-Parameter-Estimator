e84408a90859ee13626896a1df7044e18eb82c89
He Haoshen <haoshen0226@gmail.com>
2024-03-04 13:13:16 +0800

    Refactor code to generate samples from the skew normal distribution and compute parameters

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from SNParameterEstimator import ParameterEstimator
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import skewnorm
-from scipy import special
 
 # Define the number of samples, the location, the scale, and the skewness
 num_samples = 10000
@@ -12,22 +11,18 @@ mu_data = 1.0  # location (mean)
 sd_data = 5.0  # scale (standard deviation) 
 alpha_data = 5.0  # skewness (negative value means left skew, positive means right skew)
 
-# Generate the skew-normal dataset
+# Generate samples from the skew normal distribution
 dist = skewnorm(alpha_data, mu_data, sd_data)
 samples = dist.rvs(num_samples)
-
-# Create a histogram of the samples
 plt.hist(samples, bins=30, density=True, alpha=0.6, color='g')
-
-# Plot the PDF.
 xmin, xmax = plt.xlim()
 x = np.linspace(xmin, xmax, 1000)
 p = skewnorm.pdf(x, alpha_data, mu_data, sd_data)
 plt.plot(x, p, 'k', linewidth=2)
+x_values = x 
+y_values = p
 
-x_values = x  # x values are the points at which you're evaluating the PDF
-y_values = p  # y values are the corresponding values of the PDF
-
+# Computer the parameters using the function
 mu, sd, alpha = ParameterEstimator(x_values, y_values, 501)
 
 print("Estimated location (mean): ", mu)
