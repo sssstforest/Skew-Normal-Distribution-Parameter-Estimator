9e15e7e53e24bbd56a8712348a20b9dd4d10397f
He Haoshen <haoshen0226@gmail.com>
2024-03-19 11:09:07 +0800

    Update location and skewness parameters in main.py

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ from scipy.stats import skewnorm
 
 # Define the number of samples, the location, the scale, and the skewness
 num_samples = 10000
-mu_data = 1.0  # location (mean) 
+mu_data = 0.0  # location (mean) 
 sd_data = 3.0  # scale (standard deviation) 
-alpha_data = 20.0  # skewness (negative value means left skew, positive means right skew)
+alpha_data = 5.0  # skewness (negative value means left skew, positive means right skew)
 
 # Generate samples from the skew normal distribution
 dist = skewnorm(alpha_data, mu_data, sd_data)
 samples = dist.rvs(num_samples)
 plt.hist(samples, bins=30, density=True, alpha=0.6, color='g')
 xmin, xmax = plt.xlim()
+xmin = xmin + 1/7 * (xmax - xmin)
 x = np.linspace(xmin, xmax, 1000)
 p = skewnorm.pdf(x, alpha_data, mu_data, sd_data)
 plt.plot(x, p, 'k', linewidth=2)
 x_values = x 
 y_values = p
-
+plt.show()
 # Computer the parameters using the function
 mu, sd, alpha = ParameterEstimator(x_values, y_values, epochs=501)
 
